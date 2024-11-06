f88b195f567991d9f9a612c275cce49ce5d91ed5
BaoHoang2004 <nvbaohoang@gmail.com>
2024-11-06 11:13:40 +0700

    tours

--- js/script.js
+++ js/script.js
@@ -1,31 +1,43 @@
 const sidebarToggle = document.querySelector("#sidebar-toggle");
-sidebarToggle.addEventListener("click",function(){
+sidebarToggle.addEventListener("click", function () {
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
 
-document.querySelector(".theme-toggle").addEventListener("click",() => {
+document.querySelector(".theme-toggle").addEventListener("click", () => {
     toggleLocalStorage();
     toggleRootClass();
 });
 
-function toggleRootClass(){
+function toggleRootClass() {
     const current = document.documentElement.getAttribute('data-bs-theme');
     const inverted = current == 'light' ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-bs-theme',inverted);
+    document.documentElement.setAttribute('data-bs-theme', inverted);
 }
 
-function toggleLocalStorage(){
-    if(isLight()){
+function toggleLocalStorage() {
+    if (isLight()) {
         localStorage.removeItem("light");
-    }else{
-        localStorage.setItem("light","set");
+    } else {
+        localStorage.setItem("light", "set");
     }
 }
 
-function isLight(){
+function isLight() {
     return localStorage.getItem("light");
 }
 
-if(isLight()){
+if (isLight()) {
     toggleRootClass();
+}
+
+// format Tiền
+const formatCurrency = (amount) => {
+    return amount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+};
+
+
+// format ngày
+const formatDay = (day) => {
+    const newDay = new Date(day)
+    return `${newDay.getDate()}-${newDay.getMonth() + 1}-${newDay.getFullYear()}`
 }
